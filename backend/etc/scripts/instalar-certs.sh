0bd15374d3ebf16cb9a4c4440c1fc642b0823e00
Leonardo Galvao <leonardo.galvao@tre-pe.jus.br>
2026-01-21 12:47:47 -0300

    Reorg diretorios e inclusao certificiados

---
+++ backend/etc/scripts/instalar-certs.sh
@@ -0,0 +1,2 @@
+keytool -cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias cert-tre -file ../deploy/cert-tre.cer
+keytool -cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias cert-for -file ../deploy/cert-for.cer
