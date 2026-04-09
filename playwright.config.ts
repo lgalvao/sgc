e9c79b5c82f50934cfbb5cf6ad0a6159333cf1cf
Leonardo Galvao <leonardo.galvao@gmail.com>
2026-04-09 13:51:54 -0300

    Limpezas

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,14 +6,12 @@ if (process.env.NO_COLOR) {
 
 import {defineConfig, devices} from '@playwright/test';
 
-const workers = 1;
 const frontendPort = Number.parseInt(process.env.E2E_FRONTEND_PORT || '5173', 10);
 
 export default defineConfig({
     testDir: './e2e',
-    timeout: 20_000,
-    workers,
-    expect: {timeout: 4_000},
+    timeout: 15_000,
+    workers: 1,
     reporter: 'list',
     use: {
         baseURL: `http://localhost:${frontendPort}`,
