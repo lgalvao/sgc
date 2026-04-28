0c9a1d3b501119145ff90834bf31e7e139d7b5e1
Leonardo Galvao <leonardo.galvao@tre-pe.jus.br>
2026-04-28 11:00:43 -0300

    Resolve ambiente devtools

--- backend/src/main/java/sgc/Sgc.java
+++ backend/src/main/java/sgc/Sgc.java
@@ -15,6 +15,7 @@ import org.springframework.scheduling.annotation.*;
 })
 public class Sgc {
     public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(Sgc.class, args);
     }
 }
