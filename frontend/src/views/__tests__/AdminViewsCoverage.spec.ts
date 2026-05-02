34306616fb354563927df43b99060ac1b28b54a1
Leonardo Galvao <leonardo.galvao@gmail.com>
2026-05-01 22:50:29 -0300

    Melhoria cobertura e testabilidade

--- frontend/src/views/__tests__/AdminViewsCoverage.spec.ts
+++ frontend/src/views/__tests__/AdminViewsCoverage.spec.ts
@@ -1,4 +1,4 @@
-import {describe, expect, it, vi} from "vitest";
+import {describe, expect, it} from "vitest";
 import {flushPromises, mount} from "@vue/test-utils";
 import NotificacoesAdminView from "../NotificacoesAdminView.vue";
 import ProcessoCadastroView from "../ProcessoCadastroView.vue";
