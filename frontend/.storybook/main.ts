fe5ea7499a2428616f3fa53295dd866fa428601d
Leonardo Galvao <leonardo.galvao@tre-pe.jus.br>
2026-05-05 09:25:21 -0300

    feat: add Storybook integration and Vitest browser testing configuration

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -4,7 +4,8 @@ const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: [
         "@storybook/addon-links",
-        "@storybook/addon-docs"
+        "@storybook/addon-docs",
+        "@storybook/addon-vitest"
     ],
     framework: {
         name: "@storybook/vue3-vite",
