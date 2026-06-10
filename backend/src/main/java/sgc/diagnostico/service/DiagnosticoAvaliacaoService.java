4bb344ec90486d51178aa892f18942afe277293e
JoaoLRS <lucas.jlrs252291@gmail.com>
2026-06-10 12:28:46 -0300

    Realizações de acordo com o comentario no pr

--- backend/src/main/java/sgc/diagnostico/service/DiagnosticoAvaliacaoService.java
+++ backend/src/main/java/sgc/diagnostico/service/DiagnosticoAvaliacaoService.java
@@ -72,10 +72,6 @@ public class DiagnosticoAvaliacaoService {
         var avaliacoes = avaliacaoRepo.buscarAvaliacoesDoServidor(
                 diagnostico.getCodigo(), usuario.getTituloEleitoral());
 
-        if (!avaliacoes.isEmpty() && avaliacoes.get(0).getSituacaoServidor() != SituacaoAvaliacaoServidor.AUTOAVALIACAO_NAO_INICIADA) {
-            throw new ErroValidacao("A autoavaliação já foi concluída.");
-        }
-
         avaliacoes.forEach(a -> a.setSituacaoServidor(SituacaoAvaliacaoServidor.AUTOAVALIACAO_CONCLUIDA));
         avaliacaoRepo.saveAll(avaliacoes);
 
